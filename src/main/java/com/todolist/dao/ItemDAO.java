ce967981497f77d032b1504ea18c2c2c3c540941
Vlad <vkiro@yandex.ua>
2017-06-14 17:53:23 +0300

    Changed query.

--- src/main/java/com/todolist/dao/ItemDAO.java
+++ src/main/java/com/todolist/dao/ItemDAO.java
@@ -12,53 +12,36 @@ import java.util.List;
 public enum ItemDAO {
     INSTANCE;
 
-    public List<Item> getAll() throws ExceptionDAO {
-        String query = "SELECT id, text, userId FROM Items";
-        ArrayList<Item> listUsers = new ArrayList<>();
-
-        try (Connection connection = DBConnection.INSTANCE.getConnection();
-             PreparedStatement statement = connection.prepareStatement(query);
-             ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                Item item = new Item();
-                item.setId(resultSet.getInt("id"));
-                item.setText(resultSet.getString("text"));
-                item.setId(resultSet.getInt("userId"));
-                listUsers.add(item);
-            }
-        } catch (SQLException sqle) {
-            throw new ExceptionDAO("Can`t execute query", sqle);
-        }
-        return listUsers;
-    }
-
-    public Item getById(int id) throws ExceptionDAO {
-        String query = "SELECT id, text, userId FROM Items WHERE id = ?";
-        Item item;
+    public List<Item> getAllByUserId(int id) throws ExceptionDAO {
+        String query = "SELECT id, text, userId FROM Items WHERE userId = ?";
+        List<Item> items = new ArrayList<>();
         try (Connection connection = DBConnection.INSTANCE.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, id);
             try (ResultSet resultSet = statement.executeQuery()) {
-                resultSet.next();
-                item = new Item();
-                item.setId(resultSet.getInt("id"));
-                item.setText(resultSet.getString("firstName"));
-                item.setUserId(resultSet.getInt("userId"));
+                while(resultSet.next()) {
+                    Item item = new Item();
+                    item.setId(resultSet.getInt("id"));
+                    item.setText(resultSet.getString("firstName"));
+                    item.setUserId(resultSet.getInt("userId"));
+                    items.add(item);
+                }
             } catch (SQLException sqle) {
                 throw new ExceptionDAO("Can`t execute query", sqle);
             }
         } catch (SQLException sqle) {
             throw new ExceptionDAO("Can`t execute query", sqle);
         }
-        return item;
+        return items;
     }
 
-    public void createById(Item item) throws ExceptionDAO {
-        String query = "INSERT INTO Items (text, userId) VALUES (?, ?)";
+    public void create(Item item) throws ExceptionDAO {
+        String query = "INSERT INTO Items (text, userId) VALUES (?, ?) WHERE userId = ?";
         try (Connection connection = DBConnection.INSTANCE.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, item.getText());
             statement.setInt(2, item.getUserId());
+            statement.setInt(3, item.getUserId());
             statement.executeUpdate();
         } catch (SQLException sqle) {
             throw new ExceptionDAO("Can`t execute query", sqle);
@@ -78,7 +61,7 @@ public enum ItemDAO {
         }
     }
 
-    public void deleteById(Item item) throws ExceptionDAO {
+    public void delete(Item item) throws ExceptionDAO {
         String query = "DELETE FROM Items WHERE id = ?";
         try (Connection connection = DBConnection.INSTANCE.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
